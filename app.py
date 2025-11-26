71654135b5b293d02c2e3c8f9755ec1b79671b6d
Indraj Prajapat <indraj.prajapat@aitransmute.com>
2025-11-26 12:56:44 +0530

    app.py updated

--- app.py
+++ app.py
@@ -1,4 +1,17 @@
-from src.main import app
+# from src.main import app
 
-if __name__ == "__main__":
+# if __name__ == "__main__":
+#     app.run()
+
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return "<center><h1>Welcome to the Document Management System API</h1></center>"
+
+if __name__ == '__main__':
     app.run()
